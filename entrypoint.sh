d2546a630e06fe18908ad61d8b6ae2b7b66c5ef2
yujin <112833235+dbwlssid@users.noreply.github.com>
2025-04-28 16:02:16 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "<h1>Welcome to $(shuf -n1 -e 'Meow world' 'Rrrr world')</h1>" > /var/www/html/index.html
+echo "<h1>Welcome to $(shuf -n1 -e 'Meow world' 'Rrrr world')</h1>" > /opt/app-root/src/index.html
 
 httpd -DFOREGROUND
